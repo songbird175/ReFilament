8b83b8e84418ff3d61c15ea4e6d8d06daf31ef9a
songbird175 <celina.bekins@students.olin.edu>
2016-11-15 12:20:49 -0500

    Renaming things

--- GUI.py
+++ GUI.py
@@ -3,7 +3,7 @@ import tkFont
 import sys
 
 #the following code is adapted from http://python-textbok.readthedocs.io/en/1.0/Introduction_to_GUI_Programming.html
-class MyFirstGUI:
+class HomePageGUI:
     def __init__(self, master):
         self.master = master
         master.title("ReFilament PLA recycler") #shows up in the top bar
@@ -17,10 +17,10 @@ class MyFirstGUI:
         self.label.place(relx = 0.5, rely = 0.2, anchor = N)
 
         self.start_button = Button(master, text="Let's get started!", font = self.start_font, command=self.start, height = 2, width = 16)
-        self.start_button.place(relx = 0.5, rely = 0.5, anchor = CENTER) #puts the greet button in the center (taken from http://stackoverflow.com/questions/18736465/how-to-center-tkinter-wiget)
+        self.start_button.place(relx = 0.5, rely = 0.5, anchor = CENTER) #puts the start button in the center (taken from http://stackoverflow.com/questions/18736465/how-to-center-tkinter-wiget)
 
         self.log_button = Button(master, text="Check the log", font = self.log_font, command=self.log_go, height = 1, width = 12)
-        self.log_button.place(relx=0.95, rely=0.95, anchor=SE) #exit button in bottom right corner (taken from http://stackoverflow.com/questions/25396197/tkinter-put-a-widget-in-the-lower-right-corner-using-place)
+        self.log_button.place(relx=0.95, rely=0.95, anchor=SE) #log button in bottom right corner (taken from http://stackoverflow.com/questions/25396197/tkinter-put-a-widget-in-the-lower-right-corner-using-place)
 
     def start(self):
         print("This will bring you to the next page.")
@@ -31,5 +31,5 @@ class MyFirstGUI:
 
 
 root = Tk()
-my_gui = MyFirstGUI(root)
+my_gui = HomePageGUI(root)
 root.mainloop()
