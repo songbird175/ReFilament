c7e0a3a45dac32e3f5208b5f40a4b1b8a44c726f
songbird175 <celina.bekins@students.olin.edu>
2016-11-15 12:18:06 -0500

    Formatting works, now to make it pretty

--- GUI.py
+++ GUI.py
@@ -9,20 +9,24 @@ class MyFirstGUI:
         master.title("ReFilament PLA recycler") #shows up in the top bar
         master.geometry('800x480') #This seems to be good for the touchscreen, so I'm not gonna mess with it
 
-        self.header = tkFont.Font(family = 'Roboto', size = 36, weight = 'bold') #This font makes Isaac happy
-        self.font = tkFont.Font(family = 'Roboto', size = 28, weight = 'bold')
+        self.header = tkFont.Font(family = 'Roboto', size = 30, weight = 'bold') #This font makes Isaac happy
+        self.start_font = tkFont.Font(family = 'Roboto', size = 26, weight = 'bold')
+        self.log_font = tkFont.Font(family = 'Roboto', size = 18, weight = 'bold')
 
-        self.label = Label(master, text="This is our first GUI!", font = self.header) #sweet, this prints text above the buttons
-        self.label.place(relx = 0.5, rely = 0.05, anchor = N)
+        self.label = Label(master, text="So you want to recycle filament...", font = self.header) #sweet, this prints text above the buttons
+        self.label.place(relx = 0.5, rely = 0.2, anchor = N)
 
-        self.greet_button = Button(master, text="Hiii!", font = self.font, command=self.greet, height = 2, width = 8)
-        self.greet_button.place(relx = 0.5, rely = 0.5, anchor = CENTER) #puts the greet button in the center (taken from http://stackoverflow.com/questions/18736465/how-to-center-tkinter-wiget)
+        self.start_button = Button(master, text="Let's get started!", font = self.start_font, command=self.start, height = 2, width = 16)
+        self.start_button.place(relx = 0.5, rely = 0.5, anchor = CENTER) #puts the greet button in the center (taken from http://stackoverflow.com/questions/18736465/how-to-center-tkinter-wiget)
 
-        self.close_button = Button(master, text="Exit", font = self.font, command=master.quit, height = 2, width = 6)
-        self.close_button.place(relx=0.95, rely=0.95, anchor=SE) #exit button in bottom right corner (taken from http://stackoverflow.com/questions/25396197/tkinter-put-a-widget-in-the-lower-right-corner-using-place)
+        self.log_button = Button(master, text="Check the log", font = self.log_font, command=self.log_go, height = 1, width = 12)
+        self.log_button.place(relx=0.95, rely=0.95, anchor=SE) #exit button in bottom right corner (taken from http://stackoverflow.com/questions/25396197/tkinter-put-a-widget-in-the-lower-right-corner-using-place)
 
-    def greet(self):
-        print("Hey there, lovely human!")
+    def start(self):
+        print("This will bring you to the next page.")
+
+    def log_go(self):
+    	print("This will bring you to the log.")
 
 
 
