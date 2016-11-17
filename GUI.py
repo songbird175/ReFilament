15490a82bb44a0cf84c3ef2855c4c6c85ba5ba81
songbird175 <celina.bekins@students.olin.edu>
2016-11-17 16:29:39 -0500

    Trying to use ttk causes options to not be recognized

--- GUI.py
+++ GUI.py
@@ -1,4 +1,5 @@
 from Tkinter import *
+import ttk
 import tkFont
 import sys
 
@@ -18,10 +19,10 @@ class HomePageGUI:
         self.label = Label(master, text="So you want to recycle filament...", font = self.header, background = '#d8feff') #prints text above the buttons
         self.label.place(relx = 0.5, rely = 0.2, anchor = N)
 
-        self.start_button = Button(master, text="Let's get started!", font = self.start_font, command=self.start, height = 2, width = 16, background = '#06d109')
+        self.start_button = ttk.Button(master, text="Let's get started!", font = self.start_font, command=self.start, height = 2, width = 16, background = '#06d109')
         self.start_button.place(relx = 0.5, rely = 0.5, anchor = CENTER) #puts the start button in the center
 
-        self.log_button = Button(master, text="Check the log", font = self.log_font, command=self.log_go, height = 1, width = 12, background = '#06d109')
+        self.log_button = ttk.Button(master, text="Check the log", font = self.log_font, command=self.log_go, height = 1, width = 12, background = '#06d109')
         self.log_button.place(relx=0.97, rely=0.95, anchor=SE) #log button in bottom right corner
 
     def start(self):
