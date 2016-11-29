e46b35f55236dde0ea5f075f5ebfb73aba5248d7
songbird175 <celina.bekins@students.olin.edu>
2016-11-29 10:49:37 -0500

    Trying to resize and relocate log button

--- GUI.py
+++ GUI.py
@@ -1,26 +1,40 @@
-import sys
+# import sys
 from PyQt4.QtCore import *
 from PyQt4.QtGui import *
 
-def window():
-   app = QApplication(sys.argv)
-   win = QWidget()
-   win.resize(800, 480)
-	
-   prompt = QLabel()
-   prompt.setText("So you want to recycle filament...")
-   prompt.setAlignment(Qt.AlignCenter)
-	
-   vbox = QVBoxLayout()
-   vbox.addWidget(prompt)
-   # vbox.addStretch()
-	
-   prompt.setOpenExternalLinks(True)
-   win.setLayout(vbox)
-	
-   win.setWindowTitle("ReFilament")
-   win.show()
-   sys.exit(app.exec_())
-	
+class HomePage(QWidget):
+	def __init__(self):
+		QWidget.__init__(self)
+		self.setGeometry(800, 480, 800, 480)
+
+		self.prompt = QLabel()
+		self.prompt.setText("So you want to recycle filament...")
+		self.prompt.setAlignment(Qt.AlignCenter)
+		self.prompt.setOpenExternalLinks(True)
+
+		self.next_button = QPushButton("Let's get started!")
+		self.next_button.setCheckable(True)
+
+		self.log_button = QPushButton("Check the log")
+		self.log_button.setCheckable(True)
+
+		hbox = QHBoxLayout(self)
+		hbox.addStretch(1)
+		hbox.addWidget(self.log_button)
+
+		vbox = QVBoxLayout(self)
+		vbox.addLayout(hbox)
+		vbox.addStretch()
+		vbox.addWidget(self.prompt)
+		vbox.addWidget(self.next_button)
+		vbox.addStretch()
+
+		self.setWindowTitle("ReFilament")
+
 if __name__ == '__main__':
-   window()
+
+    import sys
+    app = QApplication(sys.argv)
+    window = HomePage()
+    window.show()
+    sys.exit(app.exec_())
